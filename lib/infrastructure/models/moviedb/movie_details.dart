ca7b78360d85f22fbe8728387c2a9458bd7f37fe
JavierOrdonez2001 <javier.ordonez.barra@gmail.com>
2026-07-06 22:37:16 -0400

    fix: Realizar la peticion http y probar cache

--- lib/infrastructure/models/moviedb/movie_details.dart
+++ lib/infrastructure/models/moviedb/movie_details.dart
@@ -165,13 +165,13 @@ class Genre {
 
 class ProductionCompany {
     final int id;
-    final String logoPath;
+    final String? logoPath;
     final String name;
     final String originCountry;
 
     ProductionCompany({
         required this.id,
-        required this.logoPath,
+        this.logoPath,
         required this.name,
         required this.originCountry,
     });
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatefulWidget {
+class MovieScreen extends ConsumerStatefulWidget {
 
   static const name = "movie-screen";
   final String movieId;
@@ -8,15 +10,17 @@ class MovieScreen extends StatefulWidget {
   const MovieScreen({super.key, required this.movieId});
 
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  MovieScreenState createState() => MovieScreenState();
 }
 
-class _MovieScreenState extends State<MovieScreen> {
+class MovieScreenState extends ConsumerState<MovieScreen> {
 
 
   @override
   void initState() {
     super.initState();
+    
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
   }
 
 
