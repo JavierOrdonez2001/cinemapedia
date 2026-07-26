b7b796aa83da75258e9e0ca8f816861e473fd864
JavierOrdonez2001 <javier.ordonez.barra@gmail.com>
2026-07-26 00:36:11 -0400

    feat: Mostrar actores de la pelicula & Detalles esteticos

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/providers/actors/actors_by_movie_provider.dart';
 import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
@@ -21,7 +22,6 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
 
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
     ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
-    
   }
 
   @override
@@ -67,7 +67,6 @@ class _MovieView extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
-            
             children: [
               //* Imagen redondeada
               ClipRRect(
@@ -117,8 +116,10 @@ class _MovieView extends StatelessWidget {
 
         // TODO: Mostrar actores
 
+        _ActorsByMovie(movieId: movie.id.toString()),
+
         const SizedBox(
-          height: 500,
+          height: 100,
         )
       ],
     );
@@ -139,34 +140,102 @@ class _CustomSliverAppBar extends StatelessWidget {
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
-          title: Text(movie.title,
-              textAlign: TextAlign.start,
-              style: const TextStyle(color: Colors.white, fontSize: 20)),
+          // title: Text(movie.title,
+          //     textAlign: TextAlign.start,
+          //     style: const TextStyle(color: Colors.white, fontSize: 20)),
           background: Stack(
-            children: [
-              // * imagen de fondo
-              SizedBox.expand(
-                child: Image.network(movie.posterPath, fit: BoxFit.cover),
-              ),
+        children: [
+          // * imagen de fondo
+          SizedBox.expand(
+            child: Image.network(movie.posterPath, fit: BoxFit.cover, loadingBuilder:((context, child, loadingProgress) {
+              if (loadingProgress != null) return const SizedBox();
+              return FadeIn(child: child);
+            }),),
+          ),
 
-              // * Gradiante
-              const SizedBox.expand(
-                child: DecoratedBox(
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: AlignmentGeometry.topLeft,
-                            end: AlignmentGeometry.bottomCenter,
-                            stops: [
-                      0.0,
-                      0.95
-                    ],
-                            colors: [
-                      Color.fromARGB(0, 0, 0, 0),
-                      Colors.black87,
-                    ]))),
-              )
-            ],
-          )),
+          // * Gradiante
+          const SizedBox.expand(
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: AlignmentGeometry.topLeft,
+                        end: AlignmentGeometry.bottomCenter,
+                        stops: [
+                  0.0,
+                  0.95
+                ],
+                        colors: [
+                  Color.fromARGB(0, 0, 0, 0),
+                  Colors.black87,
+                ]))),
+          )
+        ],
+      )),
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+
+    final actors = actorsByMovie[movieId];
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: actors!.length,
+          itemBuilder: ((context, index) {
+            final actor = actors[index];
+
+            return Container(
+              padding: EdgeInsets.all(8.0),
+              width: 135,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //* Actor Photo
+                  FadeInRight(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(40),
+                      child: Image.network(
+                        actor.profilePath,
+                        height: 180,
+                        width: 135,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+
+                  //* Actor name
+
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    actor.name,
+                    maxLines: 2,
+                  ),
+                  Text(
+                    actor.character ?? '',
+                    maxLines: 2,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        overflow: TextOverflow.ellipsis),
+                  ),
+                ],
+              ),
+            );
+          })),
     );
   }
 }
