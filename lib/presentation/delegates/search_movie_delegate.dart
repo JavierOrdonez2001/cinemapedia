df8b95535a9bf4979c40fa7b94534cc3958bff7e
JavierOrdonez2001 <javier.ordonez.barra@gmail.com>
2026-08-15 11:45:23 -0400

    fix: Construir sugerencias - Petición  HTTP

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -2,7 +2,15 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
+typedef SearchMovieCallback = Future<List<Movie>> Function( String query );
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMovieCallback searchMovie;
+
+  SearchMovieDelegate({required this.searchMovie});
+
+
+
   @override
   String get searchFieldLabel => 'Buscar película';
 
@@ -35,6 +43,18 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text('buildSuggestions');
+    return FutureBuilder(
+      future: searchMovie(query), 
+      builder: ((context, snapshot) {
+        final movies = snapshot.data ?? [];
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: ((context, index) {
+            final movie = movies[index];
+            return ListTile(title: Text(movie.title));
+          })
+          );
+      })
+      );
   }
 }
